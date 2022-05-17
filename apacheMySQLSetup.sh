bc53572fd3c20244f3209e229b68bb38aabfadeb
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-05-17 20:09:39 +0530

    Create apacheMySQLSetup.sh

---
+++ apacheMySQLSetup.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt install apache2 -y
+sudo ufw app list
+sudo ufw allow in "Apache" "OpenSSH"
+sudo ufw allow 3306
+sudo ufw allow in "OpenSSH"
+sudo ufw status
+sudo ufw enable
+sudo ufw status
+sudo apt install mysql-server -y
+sudo mysql
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by 'root';
+exit
