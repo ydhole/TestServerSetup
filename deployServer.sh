a0282ea8a2274c33715a66f1a9bab1ba977c502c
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-08-28 22:38:15 +0530

    Update deployServer.sh

--- deployServer.sh
+++ deployServer.sh
@@ -1,6 +1,6 @@
 #sudo apt update && sudo apt install apache2 -y
 #sudo ufw app list
-#sudo ufw allow in "Apache" "OpenSSH"
+#sudo ufw allow in "Apache"
 #sudo ufw allow 3306
 #sudo ufw allow in "OpenSSH"
 #sudo ufw status
@@ -18,10 +18,10 @@
 #sudo cp apache2.conf /etc/apache2/apache2.conf
 #sudo systemctl restart apache2
 #sudo chmod 777 -R /var/www/html
+#sudo mv CollegeMgmt /var/www/html
 #sudo chmod 777 -R /etc/mysql/mysql.conf.d/mysqld.cnf
 #sudo mv /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld_old.cnf
 #sudo cp mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 #sudo systemctl restart mysql
-sh createUser.sh
-sudo mv CollegeMgmt /var/www/html
-mysql -u ubuntu -p Ubuntu@123 college_mgmt < updateDB.sql
+#sh createUser.sh
+sudo mysql -u ubuntu -p Ubuntu@123 college_mgmt < updateDB.sql
