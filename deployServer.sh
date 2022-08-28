92064310ac4f8b94eb0948e33d4eeaf9ab2ce763
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-08-28 23:11:22 +0530

    Update deployServer.sh

--- deployServer.sh
+++ deployServer.sh
@@ -1,4 +1,8 @@
+
+# Install apache server
 sudo apt update && sudo apt install apache2 -y
+
+# Allow ports 80 for application, 3306 for mysql and 22 for SSH connection
 sudo ufw app list
 sudo ufw allow in "Apache"
 sudo ufw allow 3306
@@ -6,22 +10,37 @@ sudo ufw allow in "OpenSSH"
 sudo ufw status
 echo "y" | sudo ufw enable
 sudo ufw status
+
+# Install MySQL Server
 sudo apt install mysql-server -y
 sudo chmod +x secure_mysql.sh
 sh secureMySQL.sh
+
+# Install PHP 
 sudo apt install php libapache2-mod-php php-mysql -y
 php -v
 sudo a2enmod rewrite
 sudo systemctl restart apache2
+
+# Allow access for apache and mysql from remote
 sudo chmod 777 -R /etc/apache2/apache2.conf
 sudo mv /etc/apache2/apache2.conf /etc/apache2/apache2_old.conf
 sudo cp apache2.conf /etc/apache2/apache2.conf
+sudo rm /etc/apache2/apache2_old.conf
 sudo systemctl restart apache2
-sudo chmod 777 -R /var/www/html
-sudo mv CollegeMgmt /var/www/html
+
 sudo chmod 777 -R /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo mv /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld_old.cnf
 sudo cp mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo rm /etc/mysql/mysql.conf.d/mysqld_old.cnf
 sudo systemctl restart mysql
+
+# Place the code in the server html folder
+sudo chmod 777 -R /var/www/html
+sudo mv CollegeMgmt /var/www/html
+
+# Create ubuntu user in mysql, give privileges and create college_mgmt database
 sh createUser.sh
+
+# Dump data in college_mgmt db
 sudo mysql -u ubuntu -pUbuntu@123 college_mgmt < updateDB.sql
