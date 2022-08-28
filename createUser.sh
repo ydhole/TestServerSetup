be013a9ff4e2a73ff48b7d8a86112e14e34bb21d
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-08-28 17:45:55 +0530

    Create createUser.sh

---
+++ createUser.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# create new user ubuntu with password Ubuntu@123
+mysql -e "CREATE USER 'ubuntu'@'%' IDENTIFIED WITH mysql_native_password BY 'Ubuntu@123'"
+
+#Grant all privileges to the new user
+mysql -e "GRANT ALL PRIVILEGES on *.* TO 'ubuntu'@'%' WITH GRANT OPTION"
