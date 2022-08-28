711d97720ee83d8a0e49d64e93fc92f826d99487
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-08-28 22:48:02 +0530

    Update createUser.sh

--- createUser.sh
+++ createUser.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # create new user ubuntu with password Ubuntu@123
-mysql -e "CREATE USER 'ubuntu'@'%' IDENTIFIED WITH mysql_native_password BY 'Ubuntu@123'"
+sudo mysql -e "CREATE USER 'ubuntu'@'%' IDENTIFIED WITH mysql_native_password BY 'Ubuntu@123'"
 
 # Grant all privileges to the new user
-mysql -e "GRANT ALL PRIVILEGES on *.* TO 'ubuntu'@'%' WITH GRANT OPTION"
+sudo mysql -e "GRANT ALL PRIVILEGES on *.* TO 'ubuntu'@'%' WITH GRANT OPTION"
 
 # Create new Database "college_mgmt"
-mysql -e "CREATE DATABASE college_mgmt"
+sudo mysql -e "CREATE DATABASE college_mgmt"
