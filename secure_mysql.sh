290060c4779d1267b4b3fedf96bed5348f6f66a9
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-08-28 22:47:40 +0530

    Update secure_mysql.sh

--- secure_mysql.sh
+++ secure_mysql.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Make sure that NOBODY can access the server without a password
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('root') WHERE User = 'root'"
+sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('root') WHERE User = 'root'"
 # Kill the anonymous users
-mysql -e "DROP USER ''@'localhost'"
+sudo mysql -e "DROP USER ''@'localhost'"
 # Because our hostname varies we'll use some Bash magic here.
-mysql -e "DROP USER ''@'$(hostname)'"
+sudo mysql -e "DROP USER ''@'$(hostname)'"
 # Kill off the demo database
-mysql -e "DROP DATABASE test"
+sudo mysql -e "DROP DATABASE test"
 # Make our changes take effect
-mysql -e "FLUSH PRIVILEGES"
+sudo mysql -e "FLUSH PRIVILEGES"
 # Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
