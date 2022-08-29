6e39d2b3254266261784782bc61249b89458b908
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-08-29 19:28:40 +0530

    Update secureMySQL.sh

--- secureMySQL.sh
+++ secureMySQL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make sure that NOBODY can access the server without a password
-sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('root') WHERE User = 'root'"
+sudo mysql -e "UPDATE mysql.user SET Password = 'root' WHERE User = 'root'"
 # Kill the anonymous users
 sudo mysql -e "DROP USER ''@'localhost'"
 # Because our hostname varies we'll use some Bash magic here.
